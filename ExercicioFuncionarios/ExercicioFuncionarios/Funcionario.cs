7ef65baaf292360d0476d7a998bae57c0c28c7a7
Danilo <didox_59@yahoo.com.br>
2020-05-01 00:23:31 -0300

    novos

---
+++ ExercicioFuncionarios/ExercicioFuncionarios/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioFuncionarios
+{
+    public class Funcionario
+    {
+        public Funcionario()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public double Salario { get; set; }
+
+        public void Salvar()
+        {
+            Funcionario.BancoDeDadosEmMemoriaFuncionarios.Add(this);
+        }
+
+        public static double FolhaPagamento()
+        {
+            double valor = 0;
+            foreach(var func in Funcionario.BancoDeDadosEmMemoriaFuncionarios)
+            {
+                valor += func.Salario;
+            }
+            return valor;
+        }
+
+        public static List<Funcionario> BancoDeDadosEmMemoriaFuncionarios = new List<Funcionario>();
+    }
+}
---
+++ ExercicioFuncionarios/ExercicioFuncionarios/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ExercicioFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("O que vc deseja fazer");
+                Console.WriteLine("1 - Cadastrar funcionário");
+                Console.WriteLine("2 - Listar funcionário");
+                Console.WriteLine("3 - Mostrar folha de pagamento");
+                Console.WriteLine("4 - Sair");
+
+                bool sair = false;
+
+                try
+                {
+                    var item = Convert.ToInt32(Console.ReadLine());
+                    switch (item)
+                    {
+                        case 1:
+                            CadastarFuncionario();
+                            break;
+                        case 2:
+                            ListaFuncionario();
+                            break;
+                        case 3:
+                            MostrarFolha();
+                            break;
+                        case 4:
+                            sair = true;
+                            break;
+                    }
+
+                    if (sair) break;
+                }
+                catch
+                {
+                    Console.WriteLine("Opção inválida");
+                }
+
+            }
+        }
+
+        private static void MostrarFolha()
+        {
+            Console.WriteLine($"João sua folha de pagamento é {Funcionario.FolhaPagamento()}");
+        }
+
+        private static void CadastarFuncionario()
+        {
+            var fun = new Funcionario();
+
+            Console.WriteLine("Digite o nome");
+            fun.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o matricula");
+            fun.Matricula = Console.ReadLine();
+
+            Console.WriteLine("Digite o Salario");
+            fun.Salario = Convert.ToDouble(Console.ReadLine());
+
+            fun.Salvar();
+        }
+
+        private static void ListaFuncionario()
+        {
+            foreach(var fun in Funcionario.BancoDeDadosEmMemoriaFuncionarios)
+            {
+                Console.WriteLine("==========");
+                Console.WriteLine($"Nome: {fun.Nome}");
+                Console.WriteLine($"Salario: {fun.Salario}");
+            }
+        }
+    }
+}
